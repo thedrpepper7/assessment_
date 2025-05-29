bb5e4dbf20753437921ba7f9f9b15737eef08194
thedrpepper7 <leon.grant1410@gmail.com>
2025-05-29 15:20:43 +0100

    boom
    boom boom boom

--- js/burgermenu.js
+++ js/burgermenu.js
@@ -1,5 +1,39 @@
 const menu = document.querySelector('#burger');
+const popoutmenu = document.querySelector('#popoutmenu');
+const everythingelse = document.querySelector('#mainwrap');
+let isOpen = false;
+
+// menu.addEventListener('click', () => {
+//     if (popoutmenu.style.display === 'block') {
+
+//         popoutmenu.style.display = 'none';
+//         everythingelse.style.overflow = 'auto';
+//         everythingelse.style.position = '';
+//         everythingelse.style.left = '350px'
+
+//     } else {
+        
+//         popoutmenu.style.display = 'block';
+//         everythingelse.style.overflow = 'hidden';
+//         everythingelse.style.position = 'relative';
+//         everythingelse.style.left = '-350px';
+
+//     }
+// });
+
 
 menu.addEventListener('click', ()=> {
-    
+    popoutmenu.style.display = 'block';
+    everythingelse.style.overflow = 'hidden';
+    everythingelse.style.position = 'relative';
+    everythingelse.style.left = '-350px';
+})
+
+everythingelse.addEventListener('click', (e)=> {
+    if (e.target != menu && popoutmenu.style.display === 'block') {
+        popoutmenu.style.display = 'none';
+        everythingelse.style.overflow = 'auto';
+        everythingelse.style.position = '';
+        everythingelse.style.left = '350px';
+    }
 })
