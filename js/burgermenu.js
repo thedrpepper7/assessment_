2bc0bd9f6b293bf6c90810707c92a5bb7fe04e82
thedrpepper7 <leon.grant1410@gmail.com>
2025-05-29 15:48:52 +0100

    aslsmot fniito
    just fix the mobile layout

--- js/burgermenu.js
+++ js/burgermenu.js
@@ -1,39 +1,33 @@
 const menu = document.querySelector('#burger');
 const popoutmenu = document.querySelector('#popoutmenu');
 const everythingelse = document.querySelector('#mainwrap');
-let isOpen = false;
 
-// menu.addEventListener('click', () => {
-//     if (popoutmenu.style.display === 'block') {
+menu.addEventListener('click', () => {
+    const isOpen = popoutmenu.style.display === 'block';
 
-//         popoutmenu.style.display = 'none';
-//         everythingelse.style.overflow = 'auto';
-//         everythingelse.style.position = '';
-//         everythingelse.style.left = '350px'
-
-//     } else {
-        
-//         popoutmenu.style.display = 'block';
-//         everythingelse.style.overflow = 'hidden';
-//         everythingelse.style.position = 'relative';
-//         everythingelse.style.left = '-350px';
-
-//     }
-// });
-
-
-menu.addEventListener('click', ()=> {
-    popoutmenu.style.display = 'block';
-    everythingelse.style.overflow = 'hidden';
-    everythingelse.style.position = 'relative';
-    everythingelse.style.left = '-350px';
-})
-
-everythingelse.addEventListener('click', (e)=> {
-    if (e.target != menu && popoutmenu.style.display === 'block') {
+    if (isOpen) {
         popoutmenu.style.display = 'none';
         everythingelse.style.overflow = 'auto';
         everythingelse.style.position = '';
         everythingelse.style.left = '350px';
+        menu.classList.remove('burgerclose');
+    } else {
+        popoutmenu.style.display = 'block';
+        everythingelse.style.overflow = 'hidden';
+        everythingelse.style.position = 'relative';
+        everythingelse.style.left = '-350px';
+        menu.classList.add('burgerclose');
     }
-})
+});
+
+
+everythingelse.addEventListener('click', (e) => {
+    const isOpen = popoutmenu.style.display === 'block';
+    if (isOpen && e.target !== popoutmenu && e.target !== menu) {
+        popoutmenu.style.display = 'none';
+        everythingelse.style.overflow = 'auto';
+        everythingelse.style.position = '';
+        everythingelse.style.left = '350px';
+        menu.classList.remove('burgerclose');
+    }
+});
