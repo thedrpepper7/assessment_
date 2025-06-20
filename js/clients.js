ed87f662156c24c6a2654ee70a14804bddc51819
thedrpepper7 <leon.grant1410@gmail.com>
2025-06-20 12:33:14 +0100

    did the carousel

---
+++ js/clients.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.clients').slick({
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    draggable: false,
+    infinite: true,
+  });
+});
