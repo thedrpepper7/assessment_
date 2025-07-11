599883f2257c3f1bd5d07859da5f3389e8f88784
thedrpepper7 <leon.grant1410@gmail.com>
2025-07-11 15:58:19 +0100

    folders organised
    have to fix the css

--- views/contact.view.php
+++ views/contact.view.php
@@ -3,6 +3,11 @@
         <p><strong>Home</strong> / Our Offices</p>
     </div>
     <div class="our_offices">
-        
+        <p>Our Offices</p>
+        <div id="our_offices_image_tiles">
+            <div id="Cambridge_Office">hello</div>
+            <div id="Wymondham Office">hello</div>
+            <div id="Great Yarmouth Office">ggege</div>
+        </div>
     </div>
 </div>
