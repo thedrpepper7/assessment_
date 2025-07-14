03d22d46f15fbd0d25973e6510f8db78f49b7065
thedrpepper7 <leon.grant1410@gmail.com>
2025-07-14 14:12:26 +0100

    did the location tiles
    now onto the information enter

--- views/contact.view.php
+++ views/contact.view.php
@@ -1,13 +1,53 @@
+<div class="page_location">
+    <p class="contentwrap"><strong>Home</strong> / Our Offices</p>
+</div>
+
 <div class="contentwrap">
-    <div class="page_location">
-        <p><strong>Home</strong> / Our Offices</p>
-    </div>
     <div class="our_offices">
-        <p>Our Offices</p>
+        <h1>Our Offices</h1>
         <div id="our_offices_image_tiles">
-            <div id="Cambridge_Office">hello</div>
-            <div id="Wymondham Office">hello</div>
-            <div id="Great Yarmouth Office">ggege</div>
+            <div id="cambridge_office">
+                <img src="../img/contactPageLocations/cambridge.jpg">
+                <div id="cambridge_office_details">
+                    <p><a href="#" class="location_link">Cambridge Office</a></p>
+                    <p>Unit 1.31,
+                        St John's Innovation Centre,
+                        Cowley Road, Milton, 
+                        Cambridge,
+                        CB4 0WS
+                    </p>
+                    <p><a href="#" class="location_number"><strong>01223 37 57 72</strong></a></p>
+                    <p><a href="#" class="test">View More</a></p>
+                </div>
+            </div>
+            <div id="wymondham_office">
+                <img src="../img/contactPageLocations/wymondham.jpg">
+                <div id="wymondham_office_details">
+                    <p><a href="#" class="location_link">Wymondham Office</a></p>
+                    <p>Unit 15,
+                        Penfold Drive,
+                        Gateway 11 Business Park,
+                        Wymondham, Norfolk,
+                        NR18 0WZ
+                    </p>
+                    <p><a href="#" class="location_number"><strong>01603 70 40 20</strong></a></p>
+                    <p><a href="#" class="test">View More</a></p>
+                </div>
+            </div>
+            <div id="great_yarmouth_office">
+                <img src="../img/contactPageLocations/yarmouth-2.jpg">
+                <div id="great_yarmouth_office_details">
+                    <p><a href="#" class="location_link">Great Yarmouth Office</a></p>
+                    <p>Suite F23
+                        Beacon Innovation Centre,
+                        Beacon Park, Gorleston,
+                        Great Yarmouth, Norfolk,
+                        NR31 7RA
+                    </p>
+                    <p><a href="#" class="location_number"><strong>01493 60 32 04</strong></a></p>
+                    <p><a href="#" class="test">View More</a></p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
