3dcc8ece35212e23696d41d5f10b93eef721f7ec
thedrpepper7 <leon.grant1410@gmail.com>
2025-07-14 16:34:07 +0100

    finished html
    now moving onto the js

--- views/contact.view.php
+++ views/contact.view.php
@@ -1,53 +1,117 @@
 <div class="page_location">
-    <p class="contentwrap"><strong>Home</strong> / Our Offices</p>
+  <p class="contentwrap"><strong>Home</strong> / Our Offices</p>
 </div>
 
 <div class="contentwrap">
-    <div class="our_offices">
-        <h1>Our Offices</h1>
-        <div id="our_offices_image_tiles">
-            <div id="cambridge_office">
-                <img src="../img/contactPageLocations/cambridge.jpg">
-                <div id="cambridge_office_details">
-                    <p><a href="#" class="location_link">Cambridge Office</a></p>
-                    <p>Unit 1.31,
-                        St John's Innovation Centre,
-                        Cowley Road, Milton, 
-                        Cambridge,
-                        CB4 0WS
-                    </p>
-                    <p><a href="#" class="location_number"><strong>01223 37 57 72</strong></a></p>
-                    <p><a href="#" class="test">View More</a></p>
-                </div>
-            </div>
-            <div id="wymondham_office">
-                <img src="../img/contactPageLocations/wymondham.jpg">
-                <div id="wymondham_office_details">
-                    <p><a href="#" class="location_link">Wymondham Office</a></p>
-                    <p>Unit 15,
-                        Penfold Drive,
-                        Gateway 11 Business Park,
-                        Wymondham, Norfolk,
-                        NR18 0WZ
-                    </p>
-                    <p><a href="#" class="location_number"><strong>01603 70 40 20</strong></a></p>
-                    <p><a href="#" class="test">View More</a></p>
-                </div>
-            </div>
-            <div id="great_yarmouth_office">
-                <img src="../img/contactPageLocations/yarmouth-2.jpg">
-                <div id="great_yarmouth_office_details">
-                    <p><a href="#" class="location_link">Great Yarmouth Office</a></p>
-                    <p>Suite F23
-                        Beacon Innovation Centre,
-                        Beacon Park, Gorleston,
-                        Great Yarmouth, Norfolk,
-                        NR31 7RA
-                    </p>
-                    <p><a href="#" class="location_number"><strong>01493 60 32 04</strong></a></p>
-                    <p><a href="#" class="test">View More</a></p>
-                </div>
-            </div>
+  <div class="our_offices">
+    <h1>Our Offices</h1>
+    <div id="our_offices_image_tiles">
+      <div id="cambridge_office">
+        <img src="../img/contactPageLocations/cambridge.jpg" />
+        <div id="cambridge_office_details">
+          <p><a href="#" class="location_link">Cambridge Office</a></p>
+          <p>
+            Unit 1.31, St John's Innovation Centre, Cowley Road, Milton,
+            Cambridge, CB4 0WS
+          </p>
+          <p>
+            <a href="#" class="location_number"
+              ><strong>01223 37 57 72</strong></a
+            >
+          </p>
+          <p><a href="#" class="test">View More</a></p>
         </div>
+      </div>
+      <div id="wymondham_office">
+        <img src="../img/contactPageLocations/wymondham.jpg" />
+        <div id="wymondham_office_details">
+          <p><a href="#" class="location_link">Wymondham Office</a></p>
+          <p>
+            Unit 15, Penfold Drive, Gateway 11 Business Park, Wymondham,
+            Norfolk, NR18 0WZ
+          </p>
+          <p>
+            <a href="#" class="location_number"
+              ><strong>01603 70 40 20</strong></a
+            >
+          </p>
+          <p><a href="#" class="test">View More</a></p>
+        </div>
+      </div>
+      <div id="great_yarmouth_office">
+        <img src="../img/contactPageLocations/yarmouth-2.jpg" />
+        <div id="great_yarmouth_office_details">
+          <p><a href="#" class="location_link">Great Yarmouth Office</a></p>
+          <p>
+            Suite F23 Beacon Innovation Centre, Beacon Park, Gorleston, Great
+            Yarmouth, Norfolk, NR31 7RA
+          </p>
+          <p>
+            <a href="#" class="location_number"
+              ><strong>01493 60 32 04</strong></a
+            >
+          </p>
+          <p><a href="#" class="test">View More</a></p>
+        </div>
+      </div>
     </div>
+  </div>
+  <div id="contact_form">
+    <div id="form_fillout">
+      <div id="contact_name" class="form_entry">
+        <label for="name_input" class="required">Your Name</label>
+        <input type="text" id="name_input" />
+      </div>
+      <div id="contact_company" class="form_entry">
+        <label for="company_name_input">Company Name</label>
+        <input type="text" id="company_name_input" />
+      </div>
+      <div id="contact_email" class="form_entry">
+        <label for="email_input" class="required">Your Email</label>
+        <input type="text" id="email_input" />
+      </div>
+      <div id="contact_telephone" class="form_entry">
+        <label for="telephone_input" class="required"
+          >Your Telephone Number</label
+        >
+        <input type="text" id="telephone_input" />
+      </div>
+      <div id="contact_message" class="form_entry">
+        <label for="message_input" class="required">Message</label>
+        <textarea id="message_input"></textarea>
+      </div>
+      <div id="marketing_info_box">
+        <span id="checkbox"></span>
+        <p>
+          Please tick this box if you wish to receive marketing information from
+          us. Please see our <a href="#">Privacy Policy</a> for more information
+          on how we keep your data safe.
+        </p>
+      </div>
+      <div id="send_info">
+        <button>SEND ENQUIRY</button>
+        <p>Fields Required</p>
+      </div>
+    </div>
+    <div id="contact_information">
+      <div class="email_information">
+        <p>Email us on:</p>
+        <p><a href="#">sales@netmatters.com</a></p>
+      </div>
+      <div class="sales_information">
+        <p>Speak to Sales on:</p>
+        <p><a href="#">01603 515007</a></p>
+      </div>
+      <div class="business_hours">
+        <p>Business hours:</p>
+        <p>Monday - Friday 07:00 - 18:00</p>
+        <p id="support_dropdown">Out of Hours IT Support</p>
+      </div>
+      <div id="dropdown_content">
+        <p>hello</p>
+        <p>hello</p>
+        <p>hello</p>
+      </div>
+    </div>
+  </div>
 </div>
--- views/partials/scripts.php
+++ views/partials/scripts.php
@@ -1,8 +1,8 @@
-<script src="../js/cookies.js"></script>
+<script src="../../js/universal/cookies.js"></script>
 <script src="../js/popoutmenu.js"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="../js/imageslider.js"></script>
 <script src="../js/clients.js"></script>
 <script src="../js/vendor//swiper-bundle.min.js"></script>
 <script src="../js/vendor/slick.min.js"></script>
-<script src="../js/stickyheader.js"></script> 
+<script src="../js/stickyheader.js"></script>
