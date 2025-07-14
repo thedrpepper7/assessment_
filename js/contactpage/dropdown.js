55044596299c58283d0aab5971f7f9b12025dc42
thedrpepper7 <leon.grant1410@gmail.com>
2025-07-14 17:20:52 +0100

    content added
    dropdown box has vcontent inside it now

--- js/contactpage/dropdown.js
+++ js/contactpage/dropdown.js
@@ -1,12 +1,6 @@
 const dropdownBTN = document.querySelector("#support_dropdown");
-dropdownBTN.addEventListener("click", () => {
-  var dropdownContent = document.querySelector(".dropdown_content");
+const dropdownContent = document.querySelector(".dropdown_content");
 
-  if (dropdownContent.classList.contains("close")) {
-    dropdownContent.classList.remove("close");
-    dropdownContent.classList.add("open");
-  } else if (dropdownContent.classList.contains("open")) {
-    dropdownContent.classList.remove("open");
-    dropdownContent.classList.add("close");
-  }
+dropdownBTN.addEventListener("click", () => {
+  dropdownContent.classList.toggle("open");
 });
--- views/contact.view.php
+++ views/contact.view.php
@@ -108,9 +108,12 @@
         <p id="support_dropdown">Out of Hours IT Support</p>
       </div>
       <div class="dropdown_content close">
-        <p>hello</p>
-        <p>hello</p>
-        <p>hello</p>
+        <p>Netmatters IT are offering an Out of Hours service for Emergency and Critical tasks.</p>
+        <p>Monday - Friday 18:00 - 22:00 Saturday</</p>
+        <p>08:00 - 16:00</</p>
+        <p>Sunday 10:00 - 18:00</</p>
+        <p>To log a critical task, you will need to call our main line number and select Option 2 to leave an Out of Hours voicemail. 
+            A technician will contact you on the number provided within 45 minutes of your call.</p>
       </div>
     </div>
   </div>
